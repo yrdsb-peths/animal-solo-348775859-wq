c404ec541c66fbca7b08ae849393fcbfb1221530
348775859-wq <348775859@gapps.yrdsb.ca>
2026-05-07 11:58:46 -0400

    added score tracking system

--- MyWorld.java
+++ MyWorld.java
@@ -1,15 +1,34 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
+        
+        // create a 600x400 world
         super(600, 400, 1);
         
+        // create an Elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
+        
+        // create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
     }
     
+    //increase score
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    
+    // create an apple object at random location
     public void createApple()
     {
         Apple apple = new Apple();
