88a7f8d92d53f6a4506f20dd684f278737d1ef8f
348775859-wq <348775859@gapps.yrdsb.ca>
2026-05-08 11:25:03 -0400

    text to screen and special character p1

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,11 +18,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
     }
-    
-    
+
     // main world act loop
     public void act()
     {
@@ -32,5 +32,33 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,443,133);
+        elephant.setLocation(437,157);
+        elephant.setLocation(426,139);
+        elephant.setLocation(456,131);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,178,317);
+        label.setLocation(332,325);
+        Label label2 = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label2,145,254);
+        label2.setLocation(367,253);
+        label.setLocation(339,323);
+        label2.setLocation(269,249);
+        label.setLocation(275,308);
+        label2.setLocation(314,252);
+        label.setLocation(347,315);
+        label2.setLocation(299,250);
+        label.setLocation(254,306);
+        label.setLocation(279,304);
+        label.setLocation(297,304);
     }
 }
